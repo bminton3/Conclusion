35daa397a071e1ecf577362450333a6bbc752eff
bminton3 <ben.minton@gmail.com>
2015-10-21 21:02:20 -0500

    add styling :

--- client/main.js
+++ client/main.js
@@ -5,7 +5,7 @@ myApp.config(function ($routeProvider) {
     .when('/', {templateUrl: 'partials/home.html', access: {restricted: true}})
     .when('/login', {templateUrl: 'partials/login.html', controller: 'loginController', access: {restricted: false}})
     .when('/logout', {controller: 'logoutController', access: {restricted: true}})
-    .when('/register', {templateUrl: 'partials/register.html', controller: 'registerController', access: {restricted: true}})
+    .when('/register', {templateUrl: 'partials/register.html', controller: 'registerController', access: {restricted: false}})
     .when('/one', {template: '<h1>This is page one!</h1>', access: {restricted: true}})
     .when('/two', {template: '<h1>This is page two!</h1>', access: {restricted: false}})
     .otherwise({redirectTo: '/'});
