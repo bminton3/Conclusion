46e0b227cc295afab8b64206d2cd896c08362b56
bminton3 <ben.minton@gmail.com>
2015-11-28 22:51:18 -0500

    added navbar. Made small todo list in readme

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Conclusion
 Conclusion website. Alex Durham and Ben Minton
+
+to start:
+sudo screen mongod
+sudo screen nodemon start
+
+if mongod was shutdown uncleanly:
+sudo screen mongod --repair
+
+and then:
+sudo screen mongod
+
+TODO:
+user profile page
+migrate to AWS? (eliminate mongodb)
+finalize look and feel
+
--- client/main.js
+++ client/main.js
@@ -3,6 +3,7 @@ var myApp = angular.module('myApp', ['ngRoute']);
 myApp.config(function ($routeProvider) {
   $routeProvider
     .when('/', {templateUrl: 'partials/home.html', access: {restricted: true}})
+	.when('/navbar', {templateUrl: 'partials/navbar.html', access: {restricted: false}})
     .when('/login', {templateUrl: 'partials/login.html', controller: 'loginController', access: {restricted: false}})
     .when('/logout', {controller: 'logoutController', access: {restricted: true}})
     .when('/register', {templateUrl: 'partials/register.html', controller: 'registerController', access: {restricted: false}})
